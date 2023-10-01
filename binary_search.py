8f0ffa6a104a27f0c0d8bc814c3929a9d2af67d7
Harjot-Kaur29 <111877582+Harjot-Kaur29@users.noreply.github.com>
2023-10-01 13:58:49 +0530

    Add files via upload

---
+++ binary_search.py
@@ -0,0 +1,19 @@
+def binary_search(arr,target):
+    left = 0
+    right = len(arr)-1
+
+    while left<=right:
+        mid = (left+right)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid+1
+        else:
+            right = mid-1
+
+array = [2,4,6,7,9,0]
+result = binary_search(array,3)
+print(result)
+
+
+
